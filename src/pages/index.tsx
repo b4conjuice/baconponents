da2f41680cb2290b004a51f02596a4f4ea72246e
Daniel Lopez <dlopez807@gmail.com>
2023-01-24 16:08:25 -0800

    add db and auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,49 @@
 import { type NextPage } from 'next'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import {
+  ArrowRightOnRectangleIcon,
+  ArrowLeftOnRectangleIcon,
+} from '@heroicons/react/24/solid'
 
 import Page from '@/components/page'
 import Main from '@/components/design/main'
 import Title from '@/components/design/title'
+import Messages from '@/components/messages'
 import { api } from '@/lib/api'
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: 'from tRPC' })
+  const { data: session } = useSession()
 
   return (
     <Page>
       <Main className='flex flex-col p-4'>
+        <div className='flex justify-end space-x-4'>
+          {session ? (
+            <button
+              type='button'
+              onClick={() => {
+                signOut().catch(err => console.log(err))
+              }}
+            >
+              <ArrowLeftOnRectangleIcon className='h-6 w-6' />
+            </button>
+          ) : (
+            <button
+              type='button'
+              onClick={() => {
+                signIn('discord').catch(err => console.log(err))
+              }}
+            >
+              <ArrowRightOnRectangleIcon className='h-6 w-6' />
+            </button>
+          )}
+        </div>
         <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
           <Title>home</Title>
           {hello.data?.greeting}
         </div>
+        <Messages />
       </Main>
     </Page>
   )
